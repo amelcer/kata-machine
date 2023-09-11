cdf833356bec4f285054a1c57197fa3e26f9efbd
Andrzej Melcer <an.melcer@gmail.com>
2023-09-11 21:21:27 +0200

    Lists

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,109 @@
 export default class ArrayList<T> {
     public length: number;
+    private capacity: number;
+    private array: Array<T | undefined>;
 
-    
-
-    constructor() {
+    constructor(capacity: number) {
+        this.length = 0;
+        this.capacity = capacity;
+        this.array = new Array<T | undefined>(capacity);
     }
 
     prepend(item: T): void {
+        const arrayCopy = this.array.slice();
+        this.array[0] = item;
 
-}
+        if (this.length >= this.capacity) {
+            this.capacity += this.capacity;
+            this.array = new Array<T | undefined>(this.capacity);
+        }
+
+        this.length++;
+        for (let i = 1; i < this.length; i++) {
+            this.array[i] = arrayCopy[i - 1];
+        }
+    }
+
     insertAt(item: T, idx: number): void {
+        if (idx >= this.length || idx < 0) {
+            return;
+        }
 
-}
+        const arrayCopy = this.array.slice();
+        this.array[idx] = item;
+
+        if (idx > 0) {
+            for (let i = 0; i < idx; i++) {
+                this.array[i] = arrayCopy[i];
+            }
+        }
+
+        if (idx !== this.length - 1) {
+            for (let i = idx + 1; i < idx; i++) {
+                this.array[i] = arrayCopy[i];
+            }
+        }
+    }
+
     append(item: T): void {
+        if (this.length >= this.capacity) {
+            const arrayCopy = this.array.slice();
 
-}
+            this.capacity += this.capacity;
+            this.array = new Array<T | undefined>(this.capacity);
+
+            for (let i = 0; i < this.length; i++) {
+                this.array[i] = arrayCopy[i];
+            }
+        }
+
+        this.array[this.length] = item;
+
+        this.length++;
+    }
+
     remove(item: T): T | undefined {
+        let element;
+        let elementIndex;
+        for (let i = 0; i < this.length; i++) {
+            if (this.array[i] === item) {
+                element = item;
+                elementIndex = i;
+                break;
+            }
+        }
 
-}
+        const arrayCopy = this.array.slice();
+        if (typeof elementIndex === "number" && elementIndex >= 0) {
+            for (let i = elementIndex; i < this.length; i++) {
+                this.array[i] = arrayCopy[i + 1] || undefined;
+            }
+            this.length--;
+        }
+
+        return element;
+    }
+
     get(idx: number): T | undefined {
+        if (idx >= this.length || idx < 0) {
+            return undefined;
+        }
 
-}
+        return this.array[idx];
+    }
+
     removeAt(idx: number): T | undefined {
+        if (idx >= this.length || idx < 0) {
+            return undefined;
+        }
+        const arrayCopy = this.array.slice();
+        const element = this.array[idx];
 
-}
+        for (let i = idx; i < this.length; i++) {
+            this.array[i] = arrayCopy[i + 1] || undefined;
+        }
+
+        this.length--;
+        return element;
+    }
 }
--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,48 @@
+type Node<T> = {
+    next?: Node<T>;
+    value: T;
+};
+
 export default class Stack<T> {
     public length: number;
 
-    
+    private head: Node<T> | undefined;
 
     constructor() {
+        this.length = 0;
     }
 
     push(item: T): void {
+        this.length++;
+        if (!this.head) {
+            this.head = {
+                value: item,
+            };
 
-}
+            return;
+        }
+
+        const node: Node<T> = {
+            value: item,
+            next: this.head,
+        };
+
+        this.head = node;
+    }
+
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+        this.length--;
 
-}
+        const value = this.head.value;
+        this.head = this.head.next;
+
+        return value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
